46ebf833c6130ac5db09b8149f08a086bdf44245
BrayanGuzman01 <111928338+BrayanGuzman01@users.noreply.github.com>
2022-09-13 21:28:44 -0500

    primer commit

---
+++ Frontend/Pages/Login/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Frontend.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
